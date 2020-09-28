a50139c6672d096ddeb83e8fc091d90a17e2e783
Robert Baboi <robert.baboi@gmail.com>
2020-09-29 00:44:16 +0100

    The wait is broken for some reason

--- travis/kubernetes/wait.sh
+++ travis/kubernetes/wait.sh
@@ -4,6 +4,8 @@ namespace=$(echo $1 | tr -cd "'[:alnum:]")
 
 echo -e "\n====== Waiting for environment to become available: ${namespace} ======"
 
+set -x
+
 frontend_ip=$(kubectl -n ${namespace} get service devops-test -o json | jq -r '.status.loadBalancer.ingress[0].ip')
 
 until $(curl --output /dev/null --silent --head --fail http://${frontend_ip}); do
