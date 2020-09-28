4315cb86086cc8338ccb3c2c48b1e6ec7458dcb5
Robert Baboi <robert.baboi@gmail.com>
2020-09-28 01:20:49 +0100

    Fixing deployment script - more time

--- travis/start.sh
+++ travis/start.sh
@@ -9,9 +9,9 @@ echo -e "\n====== Starting environment: ${name} ======"
 
 gcloud compute instance-groups managed wait-until --stable --zone=${zone} ${instance_group_name}
 
-# I don't know why but sometimes it does't work, so wait a bit
+# I don't know why but sometimes it does't work, so wait a bit (the frontend takes time to become available anyway)
 # it should be ok, the file copy works fine
-sleep 10
+sleep 30
 
 for instance in $(gcloud compute instance-groups list-instances ${instance_group_name} --zone=${zone} --format="value(NAME)"); do
 
